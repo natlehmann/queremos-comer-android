48263320c3f7e7e6fdda7305bf49c9f992ca72b0
natlehmann <natlehmann@gmail.com>
2016-06-13 20:14:01 -0300

    commit inicial

---
+++ app/src/main/java/org/lehmann/natalia/queremoscomer/MenuSemanalActivity.java
@@ -0,0 +1,13 @@
+package org.lehmann.natalia.queremoscomer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuSemanalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_semanal);
+    }
+}
