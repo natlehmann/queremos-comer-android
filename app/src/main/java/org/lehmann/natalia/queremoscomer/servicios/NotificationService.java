e1149beb053ead29152bceedff89e8a3daff3186
Natalia Lehmann <natalia.lehmann@grupoesfera.com.ar>
2017-04-06 09:24:20 -0300

    cuando se toca la notificacion abre la app - por ahora levanta una unica notificacion cada vez que se abre la app

--- app/src/main/java/org/lehmann/natalia/queremoscomer/servicios/NotificationService.java
+++ app/src/main/java/org/lehmann/natalia/queremoscomer/servicios/NotificationService.java
@@ -29,83 +29,51 @@ public class NotificationService {
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         long futureInMillis = SystemClock.elapsedRealtime() + 10; // delay ????
-        AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, futureInMillis, pendingIntent);
-    }
-
-    public static Notification getNotification(String content, Context context) {
-        Notification.Builder builder = new Notification.Builder(context);
-        builder.setContentTitle("Hoy comemos");
-        builder.setContentText(content);
-        builder.setSmallIcon(R.drawable.ic_tenedor);
-        return builder.build();
     }
 
     public static Notification crearNotificacion(Context context) {
 
         RecetaCompuesta receta = MenuService.getRecetaParaAhora(context);
 
-//        if (receta != null) {
+        Intent notificationIntent = new Intent(context,
+                MenuSemanalActivity.class);
+
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                | Intent.FLAG_ACTIVITY_SINGLE_TOP); // To open only one activity
 
 
-            Intent notificationIntent = new Intent(context,
-                    MenuSemanalActivity.class);
+        Notification.Builder builder = new Notification.Builder(context);
 
-            notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    | Intent.FLAG_ACTIVITY_SINGLE_TOP); // To open only one activity
-
-
-            Notification.Builder builder = new Notification.Builder(context);
-
-            builder.setSmallIcon(R.drawable.ic_tenedor);
-            builder.setContentTitle("Hoy comemos");
-            builder.setContentText(receta.toString());
+        builder.setSmallIcon(R.drawable.ic_tenedor);
+        builder.setContentTitle("Hoy comemos");
+        builder.setContentText(receta.toString());
 //            builder.setAutoCancel(true);
 
+        Uri uri = RingtoneManager
+                .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(uri);
 
 
-            Uri uri = RingtoneManager
-                    .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-            builder.setSound(uri);
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(context,
+                MenuSemanalActivity.class);
+
+        TaskStackBuilder stackBuilder = TaskStackBuilder
+                .create(context);
+        stackBuilder.addParentStack(MenuSemanalActivity.class);
 
 
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder
+                .getPendingIntent(0, PendingIntent.FLAG_CANCEL_CURRENT);
 
-//            // Add Big View Specific Configuration
-//            NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
-//            String[] events = new String[]{receta.toString()};
-//
-//
-//            // Sets a title for the Inbox style big view
-//            inboxStyle.setBigContentTitle("Hoy comemos");
-//
-//            // Moves events into the big view
-//            for (int i = 0; i < events.length; i++) {
-//                inboxStyle.addLine(events[i]);
-//            }
-//
-//            builder.setStyle(inboxStyle);
+        builder.setContentIntent(resultPendingIntent);
 
-            // Creates an explicit intent for an Activity in your app
-//            Intent resultIntent = new Intent(context,
-//                    MenuSemanalActivity.class);
-//
-//            TaskStackBuilder stackBuilder = TaskStackBuilder
-//                    .create(context);
-//            stackBuilder.addParentStack(MenuSemanalActivity.class);
+        return builder.build();
 
-
-            // Adds the Intent that starts the Activity to the top of the stack
-
-
-//            stackBuilder.addNextIntent(resultIntent);
-//            PendingIntent resultPendingIntent = stackBuilder
-//                    .getPendingIntent(0, PendingIntent.FLAG_CANCEL_CURRENT);
-//
-//            builder.setContentIntent(resultPendingIntent);
-
-            return builder.build();
-
-//        }
     }
 
 }
